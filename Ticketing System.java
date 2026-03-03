5946159d6d7b5fa9e58b0a986b96a3828d6ff702
marvelousakinyemi910-commits <marvelousakinyemi910@gmail.com>
2026-03-03 02:19:55 +0100

    wallet method

--- Ticketing System.java
+++ Ticketing System.java
@@ -16,6 +16,29 @@ public class Ticketing System{
 }
 
 
+  public static void bookTicket(int quantity){
+
+    if (quantity <= 0){
+     System.out.println("Invalid quantitiy");
+    return;
+}
+    if (quantity > ticketAvailable){
+     System.out.println("Not enough quantity!");
+       return;
+    }
+    
+   double totalCost = quantity * ticketPrice;
+    if (userWallet < totalCost){
+        System.out.println("Insufficient wallet balance !");
+        return;
+    }
+    userWallet -= totalCost;
+    plannerWallet += totalCost;
+    ticketAvailable -= quantity;
+    System.out.println("Booking successful!");
+    System.out.println("Cost is " + totalCost);
+}
+
 }
 
 
