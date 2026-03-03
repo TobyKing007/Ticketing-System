aaf1a235ffe3ec9843c4b271b0ca97e3ba339924
Luchi <ftoluchi@gmail.com>
2026-03-03 09:02:11 +0100

    Update fundWallet.java

--- fundWallet.java
+++ fundWallet.java
@@ -1,7 +1,22 @@
-public class Ticketing System{
+import java.util.Scanner;
+public class Wallet{
+ static Scanner input = new Scanner(System.in);
+
+public static void main(String[] args){
+
+    System.out.println("Enter amount to fundWallet: ");
+    double amount = input.nextDouble();
+    
+   fundWallet(amount); 
+
+}
+
 
     public static void fundWallet(double amount){
      
+
+    int userWallet = 0;
+
 
     if (amount > 0){
     userWallet += amount;
@@ -18,6 +33,9 @@ public class Ticketing System{
 
 
   public boolean payForTicket(double amount){
+
+    int wallet = 0;
+    
     if (wallet >= amount){
         wallet -= amount;
             return true;
@@ -30,4 +48,3 @@ public class Ticketing System{
     }
     
 }
-
