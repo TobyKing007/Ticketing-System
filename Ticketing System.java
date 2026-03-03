5673d7d930f72e48d1540ce476f85cd39da9ccae
marvelousakinyemi910-commits <marvelousakinyemi910@gmail.com>
2026-03-03 01:45:54 +0100

    Tickeing System

---
+++ Ticketing System.java
@@ -0,0 +1,22 @@
+public class Ticketing System{
+
+    public static void fundWallet(double amount){
+
+    if (amount > 0){
+    userWallet += amount;
+    System.out.println("Wallet funded successfully!");
+
+    }
+    else{
+    System.out.println("Invalid amount!");
+    }
+
+
+
+}
+
+
+}
+
+
+
